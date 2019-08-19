6aa2a98d45b5d44aa158b45536e1ccfc7761a2b5
atit <atitacharya8@gmail.com>
2019-08-19 23:17:19 +0545

    admin verification done

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -98,6 +98,10 @@ class CategoriesController extends Controller
      */
     public function destroy(Category $category)
     {
+        if($category->posts->count()>0){
+            session()->flash('error', "category cannot be  deleted because it has some posts");
+        return redirect()->back();
+        }
         $category->delete();
         session()->flash('success', "category deleted successfully");
         return redirect(route("categories.index"));
--- database/migrations/2019_08_07_121122_create_posts_table.php
+++ database/migrations/2019_08_07_121122_create_posts_table.php
@@ -21,7 +21,7 @@ class CreatePostsTable extends Migration
             $table->string('image');
             $table->unsignedBigInteger('category_id');
             $table->timestamp('published_at')->nullable();
-           // $table->softDeletes();
+//            $table->softDeletes();
             $table->timestamps();
 
             $table->foreign('category_id')->references('id')->on('categories');
